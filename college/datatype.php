1bee244bd28893604e05d646307c8851a0a6fa03
Swetneel-Sheladiya <137607342+Swetneel-Sheladiya@users.noreply.github.com>
2023-07-05 15:41:17 +0530

    Add files via upload

---
+++ college/datatype.php
@@ -0,0 +1,24 @@
+<?php
+     $x = "Hello Life";
+     $y = "Hello World";
+     echo $x;
+     echo "<br/>";
+     echo $y;
+     echo "<br/>";
+
+     $a = 5985;
+     var_dump($a);
+     echo "<br/>";
+
+     $b =10.365;
+     var_dump($b);
+     echo "<br/>";
+
+     $cars = array("volvo","BMW","Toyoto");
+     var_dump($cars);
+     echo "<br/>";
+
+     $a1 = "Hello Jindagi";
+     $a1 = null;
+     var_dump($a1);
+?>     
